c8954a75146ced683789999471771f16c8392f5b
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2025-01-31 14:05:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Trailing zeroes in factorial/trailing-zeroes-in-factorial.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.trailingZeroes(N);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int trailingZeroes(int n) {
+        // Write your code here
+        int count  =0;
+        for (int i = 5;n/i>=1; i*=5) {
+            count += n/i;
+        }
+        return count;
+    }
+}
