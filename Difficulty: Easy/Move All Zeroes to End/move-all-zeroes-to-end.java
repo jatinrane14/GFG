ba364351bd60a9a774aa59b94c1838e409b57527
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2025-07-16 21:55:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -1,35 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int[] arr = new int[inputLine.length];
-            for (int i = 0; i < inputLine.length; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            new Solution().pushZerosToEnd(arr);
-            int n = arr.length;
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
@@ -37,21 +5,20 @@ class Solution {
         // code here
         int j =-1;
         for(int i =0;i<arr.length;i++){
-            if(arr[i]==0){
-                j =i;
+            if(arr[i] ==0){
+                j = i;
                 break;
             }
         }
-        for(int i =j+1;i<arr.length;i++){
-            if(j!=-1){
+        if(j>-1){
+            for(int i = j+1;i<arr.length;i++){
                 if(arr[i]!=0){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j++;
-            } 
+                    int temp =arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j++;
+                }
             }
-            
         }
     }
 }
