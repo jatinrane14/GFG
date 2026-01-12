393eae1e7784ebb312e83e36e0c4e51f97c5c7b7
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2026-01-12 21:55:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,40 +1,14 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().missingNumber(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    int missingNumber(int arr[]) {
+    int missingNum(int arr[]) {
         // code here
         long n = arr.length+1;
-        long expSum = (n*(n+1))/2;
-        long actSum =0;
-        for(int i =0;i<arr.length;i++){
-            actSum+=arr[i];
+        long expectedSum = (n*(n+1))/2;
+        long actualSum = 0 ;
+        
+        for(int elem : arr){
+            actualSum+= elem;
         }
         
-        int missNum = (int)(actSum-expSum);
-        if(missNum<0){
-            return (-1)*missNum;
-        }
-        return missNum;
+        return (int)(expectedSum - actualSum);
     }
 }
