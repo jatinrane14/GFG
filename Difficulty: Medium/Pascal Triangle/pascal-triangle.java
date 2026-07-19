47e2c58f3d32422a8b8f243b2c392ef20fcbce7c
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2026-07-19 12:14:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pascal Triangle/pascal-triangle.java
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.java
@@ -1,18 +1,14 @@
 class Solution {
-    private int findValue(int n,int r){
-        //  n -> r, r-> c
-        int res = 1;
-        for(int i=0;i<r;i++){
-            res = res * (n-i);
-            res = res/(i+1);
-        }
-        return res;
-    } 
+    
     ArrayList<Integer> nthRowOfPascalTriangle(int n) {
         // code here
         ArrayList<Integer> res = new ArrayList<>();
-        for(int c=1;c<=n;c++){
-            res.add(findValue(n-1,c-1));
+        res.add(1);
+        int ans = 1;
+        for(int c=1;c<n;c++){
+            ans = ans*(n-c);
+            ans = ans/c;
+            res.add(ans);
         }
         
         return res;
